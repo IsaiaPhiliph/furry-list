4923e0d65e6279d4ddfc23fc971d9bcececcfa13
IsaiaPhiliph <pablovalverdellamas@gmail.com>
2021-05-15 17:15:00 +0200

    .

--- hooks/api.tsx
+++ hooks/api.tsx
@@ -7,21 +7,21 @@ export const getSearchResults = async (
     setError: Function
 ) => {
     try {
-        console.log(
-            `https://api.jikan.moe/v3/search/anime?q=${search}&page=${page}`
-        );
+        // console.log(
+        //     `https://api.jikan.moe/v3/search/anime?q=${search}&page=${page}`
+        // );
         const res = await fetch(
             `https://api.jikan.moe/v3/search/anime?q=${search}&page=${page}`
         );
         const data: AnimeSearchResult = await res.json();
-        console.log(
-            `%cMaking request to api%c\nCached = ${data.request_cached.toString()}\nTime to expire cache: ${
-                data.request_cache_expiry / 60
-            }mins\nData:`,
-            "background-color:orange; color:#333; padding:5px; font-size: 1.5rem; text-transform:uppercase; font-weight:bold",
-            "background-color:green; font-size:1.2rem; font-weight:bold",
-            data
-        );
+        // console.log(
+        //     `%cMaking request to api%c\nCached = ${data.request_cached.toString()}\nTime to expire cache: ${
+        //         data.request_cache_expiry / 60
+        //     }mins\nData:`,
+        //     "background-color:orange; color:#333; padding:5px; font-size: 1.5rem; text-transform:uppercase; font-weight:bold",
+        //     "background-color:green; font-size:1.2rem; font-weight:bold",
+        //     data
+        // );
         return data;
     } catch (err) {
         setError(err.message);
@@ -31,15 +31,15 @@ export const getSearchResults = async (
 
 export const getAnimeDetails = async (id: number): Promise<any> => {
     try {
-        console.log(
-            `Making request to https://api.jikan.moe/v3/anime/${id}/ on the server`
-        );
+        // console.log(
+        //     `Making request to https://api.jikan.moe/v3/anime/${id}/ on the server`
+        // );
         const res = await fetch(`https://api.jikan.moe/v3/anime/${id}/`);
         const data = await res.json();
-        console.log(data);
+        // console.log(data);
         return data;
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         return;
     }
 };
